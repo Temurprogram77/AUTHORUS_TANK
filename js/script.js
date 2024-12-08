babee5dded43134393fe836969d063961a0467e8
Temur Coder <temurnarzullayev810@gmail.com>
2024-12-08 20:25:04 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let menu = document.querySelector('.menu');
 let menu_bar = document.querySelector('.menu_burger');
 let close = document.querySelector('.img');
+let black = document.querySelector('.black');
+let red = document.querySelector('.red');
+let car_img = document.querySelector('.red_car-img img');
 
 menu_bar.addEventListener('click', () => {
   menu.classList.add('active_menu');
@@ -8,4 +11,8 @@ menu_bar.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   menu.classList.remove('active_menu');
+});
+
+black.addEventListener('click', () => {
+  car_img.src = '/assets/img/black_car.png';
 });
