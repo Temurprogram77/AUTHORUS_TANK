6af128c200dca21b4eca57fd39fc3d9374031d39
Temur Coder <temurnarzullayev810@gmail.com>
2024-12-08 20:27:49 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-let menu = document.querySelector('.menu');
-let menu_bar = document.querySelector('.menu_burger');
-let close = document.querySelector('.img');
-let black = document.querySelector('.black');
-let red = document.querySelector('.red');
-let car_img = document.querySelector('.red_car-img img');
+let menu = document.querySelector(".menu");
+let menu_bar = document.querySelector(".menu_burger");
+let close = document.querySelector(".img");
+let black = document.querySelector(".black");
+let red = document.querySelector(".red");
+let car_img = document.querySelector(".red_car-img img");
 
-menu_bar.addEventListener('click', () => {
-  menu.classList.add('active_menu');
+menu_bar.addEventListener("click", () => {
+  menu.classList.add("active_menu");
 });
 
-close.addEventListener('click', () => {
-  menu.classList.remove('active_menu');
+close.addEventListener("click", () => {
+  menu.classList.remove("active_menu");
 });
 
-black.addEventListener('click', () => {
-  car_img.src = '/assets/img/black_car.png';
+// 
+
+black.addEventListener("click", () => {
+  car_img.src = "./assets/images/black_tank.png";
+});
+red.addEventListener("click", () => {
+  car_img.src = "./assets/images/red_car_big.png";
 });
